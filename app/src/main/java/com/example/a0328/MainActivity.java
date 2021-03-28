32e9b897ef7d74ada8452b738d33439f6fb71c69
maoderizi <damdam2020@naver.com>
2021-03-28 15:51:10 +0900

    안녕이

---
+++ app/src/main/java/com/example/a0328/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a0328;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+        });
+    }
+    void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("안녕이").setMessage("!!!");
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }}
